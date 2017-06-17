022ca005b9c82db42d81470c770a00fe9a66faa7
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2017-06-17 13:02:03 -0300

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
